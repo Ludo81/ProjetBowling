95e82364c8fba775a126755bca93c358c2127230
ltourn01 <ltourn01@pcalb-eb0120.univ-jfc.fr>
2018-10-02 15:16:58 +0200

    Complété

--- src/main/java/com/mycompany/multiplayergame/MultiGame.java
+++ src/main/java/com/mycompany/multiplayergame/MultiGame.java
@@ -17,13 +17,13 @@ public class MultiGame implements MultiPlayerGame
     String[] nomJoueur;    
     HashMap <String,SinglePlayerGame> joueurs;
     
-    public MultiGame()                              //Constructeur de notre class. Il contient toutes les variables a modifier pour le deroulement de la partie.
+    public MultiGame()                              //Constructeur
     {
-        this.nombreTotalJoueurs=0;                  //Va servir pour l'exception aucun joueur.
+        this.nombreTotalJoueurs=0;                  //Pour la vérification du nombre
         this.numJoueur=0;                           //Numero du joueur jouant actuellement
-        this.gameStarted=false;                     //Booleen verifiant l'etat du jeu (terminé ou pas)
-        this.nomJoueur= new String[0];              //Liste des noms des joueurs. Utilisé comme clé dans la table.
-        this.joueurs= new HashMap<>();              //Table formé des noms des joueurs auquel on associe la partie solo.
+        this.gameStarted=false;                     //Détermine si la partie est démarré
+        this.nomJoueur= new String[0];              //Liste des joueurs
+        this.joueurs= new HashMap<>();              //Table clé : joueur, valeur : son SinglePlayerGame
     }
     
     /**
@@ -36,21 +36,21 @@ public class MultiGame implements MultiPlayerGame
     @Override
     public String startNewGame(String[] playerName) throws Exception 
     {
-        this.nombreTotalJoueurs=playerName.length;              //Affectation des variables, nombre de joueurs, partie commencée, liste des joueurs.
+        this.nombreTotalJoueurs=playerName.length;              
         this.gameStarted=true;
         this.nomJoueur=playerName;
         
         if (nombreTotalJoueurs==0)
         {
-            throw new java.lang.Exception("Il n'y a aucun joueur.");        //Dans le cas ou la liste est vide, aucun joueur présent.
+            throw new java.lang.Exception("Il n'y a aucun joueur.");        //Si aucun joueur présent.
         }
         
         for (int i=0; i<playerName.length; i++)
         {
-            this.joueurs.put(playerName[i], new SinglePlayerGame());        //Ajoute tout les joueurs dans la table de joueurs en y associant leur partie.
+            this.joueurs.put(playerName[i], new SinglePlayerGame());        //Ajout dans table
         }
         
-        return "Prochain tir : joueur " + playerName[numJoueur] +                                               //Retourne la formule indiqué au dessus si tout se passe correctement.
+        return "Prochain tir : joueur " + playerName[numJoueur] +                                               
                 ", tour n° " + this.joueurs.get(playerName[numJoueur]).getCurrentFrame().getFrameNumber() + 
                 ", boule n° " + this.joueurs.get(playerName[numJoueur]).getCurrentFrame().getBallsThrown()+1;
     }
@@ -68,29 +68,29 @@ public class MultiGame implements MultiPlayerGame
     {
         if (!this.gameStarted)
         {
-            throw new java.lang.Exception("La partie n'est pas démarrée ou elle est terminée.");        //On verifie l'etat de la partie. Si terminé ou non commencé on lance l'exception
+            throw new java.lang.Exception("La partie n'est pas démarrée ou elle est terminée.");        
         }
         
-        this.joueurs.get(nomJoueur[numJoueur]).lancer(nombreDeQuillesAbattues);                         //On effectue le lancer pour le joueur courant.
+        this.joueurs.get(nomJoueur[numJoueur]).lancer(nombreDeQuillesAbattues);                         //Lancé pour le joueur courant.
         
         if (this.joueurs.get(nomJoueur[numJoueur]).getCurrentFrame().isFinished())
         {
-            this.numJoueur++;                                                                           //Si le lancer etait le dernier du tour alors il passera automatiquement au tour suivant(Verifier la classe SinglePlayerGame).
-        }                                                                                               //Pour ce tour ci c'est au joueur suivant de jouer.
+            this.numJoueur++;                                                                           
+        }                                                                                               
         
-        if (this.numJoueur==this.nomJoueur.length)                                                      //Plusieurs choses doivent etre verifié.
-        {                                                                                               //Premierement si on arrive au dernier joueur de la liste.
+        if (this.numJoueur==this.nomJoueur.length)                                                      
+        {                                                                                               
             if (this.joueurs.get(this.nomJoueur[this.numJoueur-1]).getCurrentFrame()==null)             //Si le tour du dernier joueur est terminé.
             {
-                this.gameStarted=false;                                                                 //Dans ce cas on arrete la partie. Retournant la formule demandé.
+                this.gameStarted=false;                                                                 //Arrêt de la partie
                 return "Partie terminée";
             }
-            this.numJoueur=0;                                                                           //Si ce n'est pas le dernier tour, dans ce cas on retourne au premier joueur.
+            this.numJoueur=0;                                                                           //Retour au premier joueur si c'est pas terminé
         }
         
         return "Prochain tir : joueur " + this.nomJoueur[this.numJoueur] + 
                 ", tour n° " + this.joueurs.get(this.nomJoueur[this.numJoueur]).getCurrentFrame().getFrameNumber() + 
-                ", boule n° " + this.joueurs.get(this.nomJoueur[this.numJoueur]).getCurrentFrame().getBallsThrown()+1;      //Par defaut on affiche la formule du prochain joueur.
+                ", boule n° " + this.joueurs.get(this.nomJoueur[this.numJoueur]).getCurrentFrame().getBallsThrown()+1;     
     }
 
     /**
@@ -102,10 +102,10 @@ public class MultiGame implements MultiPlayerGame
     @Override
     public int scoreFor(String playerName) throws Exception
     {
-        if (!this.joueurs.containsKey(playerName))                                          //Simple fonction, on verifie la presence du joueur, lançant l'exception si le nom ne figure pas dans la liste.
+        if (!this.joueurs.containsKey(playerName))      
         {
             throw new java.lang.Exception("Ce joueur ne joue pas dans cette partie.");
         }
-        return this.joueurs.get(playerName).score();                                        //Sinon on recupere par defaut le score dans la table enregistré dans l'objet associé.
+        return this.joueurs.get(playerName).score();         
     }  
 }
--- src/test/java/com/mycompany/multiplayergame/MultiGameTest.java
+++ src/test/java/com/mycompany/multiplayergame/MultiGameTest.java
@@ -6,7 +6,6 @@
 package com.mycompany.multiplayergame;
 
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -18,56 +17,56 @@ import static org.junit.Assert.*;
  */
 public class MultiGameTest
 {    
-    private MultiGame multiGame;
-    String[] empty;
-    String[] partie;
+    private MultiGame jeuMulti;
+    String[] vide;
+    String[] parties;
     
     @Before
     public void setUp() 
     {
-        multiGame=new MultiGame();
-        empty=new String[0];
-        partie=new String[] {"Jacke", "Jean", "Emilie", "Marc"};
+        jeuMulti=new MultiGame();
+        vide=new String[0];
+        parties=new String[] {"Marcel", "Patrice", "Emile", "Marco"};
     }
     
     @Test (expected=java.lang.Exception.class)
     public void testAucunJoueur() throws Exception 
     {
-        multiGame.startNewGame(empty);
+        jeuMulti.startNewGame(vide);
     }
     
     @Test (expected=java.lang.Exception.class)
     public void testPartieNonCommencé() throws Exception
     {
-        multiGame.lancer(5);
-    }
-    
-    @Test (expected=java.lang.Exception.class)
-    public void testScoreJoueurInnexistant() throws Exception
-    {
-        multiGame.startNewGame(partie);
-        assertEquals(0, multiGame.scoreFor("Michel"));
-    }
-    
-    @Test
-    public void testCreationPartie() throws Exception
-    {
-        assertEquals("Prochain tir : joueur Jacke, tour n° 1, boule n° 01", multiGame.startNewGame(partie));
+        jeuMulti.lancer(5);
     }
     
     @Test
     public void testScore() throws Exception
     {
-        multiGame.startNewGame(partie);
-        assertEquals(0, multiGame.scoreFor("Jacke"));
+        jeuMulti.startNewGame(parties);
+        assertEquals(0, jeuMulti.scoreFor("Marcel"));
+    }
+    
+    @Test (expected=java.lang.Exception.class)
+    public void testScoreJoueurInnexistant() throws Exception
+    {
+        jeuMulti.startNewGame(parties);
+        assertEquals(0, jeuMulti.scoreFor("Patrice"));
+    }
+    
+    @Test
+    public void testCreationPartie() throws Exception
+    {
+        assertEquals("Prochain tir : joueur Jacke, tour n° 1, boule n° 01", jeuMulti.startNewGame(parties));
     }
     
     @Test
     public void testRandomGame() throws Exception
     {
-        multiGame.startNewGame(partie);
-        multiGame.lancer(5);
-        assertEquals(5, multiGame.scoreFor("Jacke"));
+        jeuMulti.startNewGame(parties);
+        jeuMulti.lancer(5);
+        assertEquals(5, jeuMulti.scoreFor("Marcel"));
     }
     
 }
